f88cf5097b2a3b1665a089e6634942a0b3cc65dc
jasonbatingan1-cmd <jason.batingan1@gmail.com>
2026-05-19 18:38:40 -0700

    updates to different system files

---
+++ client/src/api.js
@@ -0,0 +1,29 @@
+const API_BASE = "http://localhost:5000/api";
+
+export async function apiGet(path) {
+    const res = await fetch(`${API_BASE}${path}`);
+    return res.json();
+}
+
+export async function apiPost(path, data) {
+    const res = await fetch(`${API_BASE}${path}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return res.json();
+}
+
+export async function apiPut(path, data) {
+    const res = await fetch(`${API_BASE}${path}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return res.json();
+}
+
+export async function apiDelete(path) {
+    const res = await fetch(`${API_BASE}${path}`, { method: "DELETE" });
+    return res.json();
+}
