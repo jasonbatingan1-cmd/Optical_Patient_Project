45e3bc670933a571dbc4307bb4ab634dde4a865d
jasonbatingan1-cmd <jason.batingan1@gmail.com>
2026-06-01 19:50:13 -0700

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // hashed
     role: { type: String, default: "user" } // user, admin, optician
