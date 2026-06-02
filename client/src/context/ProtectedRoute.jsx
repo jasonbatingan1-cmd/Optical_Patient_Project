d45e4973779afaad35e1fef9c5e697ef98a0ccda
jasonbatingan1-cmd <jason.batingan1@gmail.com>
2026-06-01 19:54:36 -0700

    Create ProtectedRoute.jsx
    Create context for RBAC

---
+++ client/src/context/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.jsx";
+
+export default function ProtectedRoute({ children }) {
+    const { isAuthenticated, loading } = useAuth();
+
+    if (loading) return <p>Loading...</p>;
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+}
